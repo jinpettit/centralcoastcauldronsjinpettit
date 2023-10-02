11fbe18876f819f5230572e2e1f1524664488eb0
Jinrong Pettit <jinpettit@gmail.com>
2023-10-01 21:41:59 -0700

    edge case for potion:

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -21,21 +21,20 @@ def post_deliver_potions(potions_delivered: list[PotionInventory]):
     print(potions_delivered)
 
     for potion in potions_delivered:
-        if (potion.potion_type[0] == 100):
-            with db.engine.begin() as connection:   
-                result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_red_potions FROM global_inventory WHERE id=1"))
+        with db.engine.begin() as connection:   
+            result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_red_potions FROM global_inventory WHERE id=1"))
 
-                data = result.fetchone()
+            data = result.fetchone()
 
-                num_red_ml = data[0]
-                num_red_potions = data[1]
+            num_red_ml = data[0]
+            num_red_potions = data[1]
 
-                curr_red_potions = potion.quantity + num_red_potions
+            curr_red_potions = potion.quantity + num_red_potions
 
-                new_num_red_ml = num_red_ml - (potion.quantity * 100)
-    
-                connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = :new_num_red_ml, num_red_potions = :curr_red_potions WHERE id=1"), 
-                            {"new_num_red_ml": new_num_red_ml,"curr_red_potions": curr_red_potions})
+            new_num_red_ml = num_red_ml - (potion.quantity * 100)
+
+            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = :new_num_red_ml, num_red_potions = :curr_red_potions WHERE id=1"), 
+                        {"new_num_red_ml": new_num_red_ml,"curr_red_potions": curr_red_potions})
 
 
     return "OK" 
