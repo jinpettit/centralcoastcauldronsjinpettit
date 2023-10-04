d117c2f01ab239a20aacd262f8d16fb940bf03a0
Jinrong Pettit <jinpettit@gmail.com>
2023-10-04 11:36:13 -0700

    first commit for asgn2

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -79,7 +79,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                             "quantity": 1,
                         })
             '''
-        return []
+        return lst
 
 
 
