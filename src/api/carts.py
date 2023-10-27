dbb3922809ea3adedd45de434986add4de5c98b4
Jinrong Pettit <jinpettit@gmail.com>
2023-10-27 03:14:05 -0700

    errors

--- src/api/carts.py
+++ src/api/carts.py
@@ -77,6 +77,7 @@ def search_orders(
 
     stmt = (sqlalchemy.select(db.carts.c.customer, db.cart_items.c.id, db.cart_items.c.created_at, db.cart_items.c.quantity, db.potion_table.c.sku, db.potion_table.c.price)
             .select_from(db.cart_items).join(db.carts, db.cart_items.c.cart_id == db.carts.c.id).join(db.potion_table, db.cart_items.c.potion_id == db.potion_table.c.id)
+            .join(db.potion_ledger, db.potion_ledger.c.potion_id == db.potion_table.c.id)
     .limit(6)
     .offset(page_number)
     .order_by(order_by))
